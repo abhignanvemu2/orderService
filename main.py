3c08a8e469a3344bbe13b8ff792b9154eee05b65
Abhi <Abhibruce2@gmail.com>
2025-06-01 00:50:59 +0530

    update port

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import uvicorn
+import os
 from fastapi import FastAPI
 from Config.db import engine, Base
 from Routes import router as ApiRouter
@@ -18,3 +20,8 @@ def on_shutdown():
     print("🛑 Database connection closed")
 
 app.include_router(ApiRouter)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8006))  # fallback to 8000
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
